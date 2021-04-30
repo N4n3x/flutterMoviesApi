5f268a088c037b9faa64ac6a1faa8acd845d52fe
Fabien <flierville@cesi.fr>
2021-04-30 11:21:15 +0200

    Interrogation API

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_api/models/api_movie.dart';
+import 'package:flutter_api/models/movie.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -10,13 +12,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Movie> movies = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Interroger API"),
+          onPressed: getPopularMovies,
+        ),
+      ),
     );
   }
+
+  Future<void> getPopularMovies() async {
+    ApiMovie api = ApiMovie();
+    Map<String, dynamic> mapMovie = await api.getPopular();
+    if(mapMovie["code"] == 0){
+      // Je peux concevoir ma liste de Movie
+      movies = Movie.moviesFromApi(mapMovie["body"]);
+      movies.forEach((Movie movie) {
+        print(movie.movieTitle);
+      });
+
+    }else{
+      //Todo afficher erreur
+    }
+
+  }
+
 }

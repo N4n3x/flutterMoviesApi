ee1da543a58e82ac75a1962edda93ca59048bfe8
Fabien <flierville@cesi.fr>
2021-04-30 10:27:26 +0200

    Class Movie

---
+++ lib/models/movie.dart
@@ -0,0 +1,13 @@
+
+class Movie {
+  final int movieId;
+  final String movieTitle;
+  final String movieOverview;
+  final DateTime movieReleaseDate;
+  final String movieImageUrl;
+  final double movieVote;
+
+  Movie({this.movieId, this.movieTitle, this.movieOverview, this.movieReleaseDate, this.movieImageUrl, this.movieVote});
+
+
+}

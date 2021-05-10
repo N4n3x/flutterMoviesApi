8d37584d41c20c74879344d429cfb393c9d7cb56
Fabien <flierville@cesi.fr>
2021-05-10 08:53:11 +0200

    Widget Chargement et Erreur

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api/models/api_movie.dart';
 import 'package:flutter_api/models/movie.dart';
+import 'package:flutter_api/widgets/chargement.dart';
+import 'package:flutter_api/widgets/erreur.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,37 +15,67 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Movie> movies = [];
+  StatusApi _statusApi;
+
+  @override
+  void initState() {
+    getPopularMovies();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: getPopularMovies
+          )
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Interroger API"),
-          onPressed: getPopularMovies,
-        ),
-      ),
+      body: choixDuBody(),
     );
   }
 
+
+  Widget choixDuBody(){
+    if(_statusApi == StatusApi.chargement){
+      return Chargement();
+    }else if(_statusApi == StatusApi.error){
+      return Erreur();
+    }else{
+      //Todo : Faire un ListView Builder puis un Grid View pour le paysage
+      return Center();
+    }
+  }
+
+
   Future<void> getPopularMovies() async {
-    print("Coucou");
+    setState(() {
+      _statusApi = StatusApi.chargement;
+    });
     ApiMovie api = ApiMovie();
     Map<String, dynamic> mapMovie = await api.getPopular();
     if(mapMovie["code"] == 0){
       // Je peux concevoir ma liste de Movie
-      movies = Movie.moviesFromApi(mapMovie["body"]);
-      movies.forEach((Movie movie) {
-        print(movie.movieTitle);
+      setState(() {
+        movies = Movie.moviesFromApi(mapMovie["body"]);
+        _statusApi = StatusApi.ok;
       });
-
     }else{
-      //Todo afficher erreur
+      setState(() {
+        _statusApi = _statusApi = StatusApi.error;
+      });
     }
 
   }
 
+}
+
+enum StatusApi {
+  chargement,
+  error,
+  ok
 }
